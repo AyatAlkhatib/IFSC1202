30db00dc4ee1b5344b421242bae7740ad549079a
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-27 17:51:41 +0000

    Project 1

---
+++ 01.Project.py
@@ -0,0 +1,28 @@
+X= "Years:0"     #X represents years 
+Y= "Weeks:48"    #Y represents weeks  
+z= "Days:4"      #z represents days
+print ("Length of Time in days:340")
+print(X) 
+print(Y)
+print(z)
+
+print ("Length of Time in days:366")
+x= "Years:1"  #x represents years 
+y= "Weeks:0"  #y represents weeks 
+Z= "Days:1"   #Z represents days 
+print(x) 
+print(y) 
+print(Z)
+
+print("Length of Time in days:1000")
+x="Years:2"    #x represents years 
+y="Weeks:38"   #y represents weeks  
+z="Days:4"     #z represents days  
+print(x)
+print(y)
+print(z)
+
+
+
+
+
