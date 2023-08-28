2e78aa972d52475aac0fc49b46db190513a1fbc0
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-28 18:33:51 +0000

    01.Project.py

--- 01.Project.py
+++ 01.Project.py
@@ -1,9 +1,10 @@
 Value =int(input("Enter Length of Time in days:"))
 years= Value//365
 print("years:",years)
-weeks=Value//years*365
+weeks=(Value//365)*52
 print("weeks:", weeks)
-
+days= (Value%365)%7
+print("days:", days)
 
 
 
