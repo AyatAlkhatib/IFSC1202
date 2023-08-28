81b4c0ed5cbda14bd764f64af557b13a974035ab
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-28 14:46:36 +0000

    Project 1

--- 01.Project.py
+++ 01.Project.py
@@ -1,23 +1,26 @@
-X= "Years:0"     #X represents years 
-Y= "Weeks:48"    #Y represents weeks  
-z= "Days:4"      #z represents days
+#x represents years
+#y represents weeks
+#z represents days
+X= "Years:0"      
+Y= "Weeks:48"    
+z= "Days:4"      
 print ("Length of Time in days:340")
 print(X) 
 print(Y)
 print(z)
 
 print ("Length of Time in days:366")
-x= "Years:1"  #x represents years 
-y= "Weeks:0"  #y represents weeks 
-Z= "Days:1"   #Z represents days 
+x= "Years:1"   
+y= "Weeks:0"   
+Z= "Days:1"   
 print(x) 
 print(y) 
 print(Z)
 
 print("Length of Time in days:1000")
-x="Years:2"    #x represents years 
-y="Weeks:38"   #y represents weeks  
-z="Days:4"     #z represents days  
+x="Years:2"   
+y="Weeks:38"     
+z="Days:4"     
 print(x)
 print(y)
 print(z)
---
+++ project1.py
@@ -0,0 +1,5 @@
+#365 days in a year 
+a= input("Enter Length of Time in Days: ")
+x= input("Years:")
+y=input("Weeks:")
+z=input("Days:")
