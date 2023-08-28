52cf6e816838d01a3d3bfa78140ac4a2f66a4ce3
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-28 15:49:36 +0000

    01.project.py

--- 01.Project.py
+++ 01.Project.py
@@ -1,29 +1,15 @@
-#x represents years
-#y represents weeks
-#z represents days
-X= "Years:0"      
-Y= "Weeks:48"    
-z= "Days:4"      
-print ("Length of Time in days:340")
-print(X) 
-print(Y)
-print(z)
+value=int(input("Enter Length of Time in days:"))
+years= value//365
+print("Years:,Years")
 
-print ("Length of Time in days:366")
-x= "Years:1"   
-y= "Weeks:0"   
-Z= "Days:1"   
-print(x) 
-print(y) 
-print(Z)
 
-print("Length of Time in days:1000")
-x="Years:2"   
-y="Weeks:38"     
-z="Days:4"     
-print(x)
-print(y)
-print(z)
+
+
+
+
+
+
+
 
 
 
