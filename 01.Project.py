cc413ea56591018a84dc906e5bfe87715da100eb
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-29 21:22:19 +0000

    01.project

--- 01.Project.py
+++ 01.Project.py
@@ -1,13 +1,12 @@
-#x represents years
-#y represents weeks 
-#z reprsents days
+
 Value =int(input("Enter Length of Time in days:"))
-x= Value//365
-print("x:",x)
-y=(Value//365)*52
-print("y:", y)
+years= Value//365
+print ("years:", years)
+weeks=(Value//365)*52
+print("weeks:", weeks)
 days= (Value%365)%7
-print("z:", z)
+print("days:",days)
+      
 
 
 
