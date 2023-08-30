64d98144daada959afa9aa15939504305319c394
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-08-30 14:14:51 +0000

    01.project.py

--- 01.Project.py
+++ 01.Project.py
@@ -1,10 +1,12 @@
+#approx 365 days in a year 
+#approx 52 weeks in a year
 
-Value =int(input("Enter Length of Time in days:"))
-years= Value//365                                    
-print ("years:", years)              
-weeks= (Value-years)//7
+Value =int(input("Enter Length of Time in days:"))         #we want the results in integiers. 
+years= int(Value//365)                 #calculate years is value of the length time in days and floor divided by the number of days in a year                   
+print("years:", years)              
+weeks= int(Value-years)//7           #calculate weeks is value minus the result of years and then floor divided by the number of days in weeks 
 print("weeks:", weeks)
-days= (365%Value)
+days= int(Value%years//weeks)
 print("days:",days)
       
 
