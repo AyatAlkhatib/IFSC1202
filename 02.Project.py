8247f8c380d69caea3e63f7ef141f11e13bee975
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-04 18:36:34 +0000

    0.2.Project.py

---
+++ 02.Project.py
@@ -0,0 +1,17 @@
+#Equailateral= all sides are equal 
+#Isoclec= two sides are equal 
+#Scalene= none of the sides are equal 
+#Calaculate the area using hebrons formula
+#A reprsents side 1
+#B reprsents side 2
+#C reprsents side 3 
+#P reprsents Perimeter 
+#S reprsents Semiperimeter 
+
+A=float(input("Enter side 1: "))     #our sides need to be in float form             
+B=float(input("Enter side 2: "))
+C=float(input("Enter side 3: "))
+P= A + B + C                           #Calculate Perimeter is Adding all three sides 
+S= P/2                                 #Calculate Semiperimeter by diving the perimeter by 2 
+area= float(S*(S-A)*(S-B)*(S-C))**0.5   #Calculate area by multiplying the s by the out comes of s minus the three sides then mulitplaying it by 0.5 #in this context 0.5 is equalivelnt to taking a squre root
+print ("area:",area)                    #the area must be float form 
