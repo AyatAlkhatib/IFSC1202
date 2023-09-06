27b5cb1f080d030aa45f278b1d7ac45d8cfc3223
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-06 14:33:17 +0000

    02.Project.py

--- 02.Project.py
+++ 02.Project.py
@@ -12,6 +12,6 @@ A=float(input("Enter side 1: "))     #our sides need to be in float form
 B=float(input("Enter side 2: "))
 C=float(input("Enter side 3: "))
 P= float(A + B + C)                           #Calculate Perimeter is Adding all three sides 
-S= float(P/2)                                 #Calculate Semiperimeter by diving the perimeter by 2 
-area= float(S*(S-A)*(S-B)*(S-C))**0.5  #Calculate area by multiplying the s by the out comes of s minus the three sides then mulitplaying it by 0.5 #in this context 0.5 is equalivelnt to taking a squre root
+S= float(P/2)                                 #Calculate Semiperimeter by diving the results of the perimeter by 2 since we want half of P
+area= float(S*(S-A)*(S-B)*(S-C))**0.5  #Calculate area by multiplying the the result S by the out comes of S minus the three sides then mulitplaying it by 0.5 #in this context 0.5 is equalivelnt to taking a squre root
 print ("area:",area)                    #the area must be float form 
