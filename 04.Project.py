afa4f8b048a1b5ec6a974e4eb52d0d804707170c
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-11 15:43:51 +0000

    03.Project.py

--- 04.Project.py
+++ 04.Project.py
@@ -1,10 +1,10 @@
 N= int(input("Enter Start of Range:"))
 N2= int(input("Enter End of Range: "))
 A= (input("Prime Numbers between 50 and 100"))
-for i in range (2, N//2 , +1):
+for i in range (N,N2//2, +1):
     print(i)
-if N % i ==0: 
-    print(N % i)
+if A % i ==0: 
+    print(A % i)
 else: 
     print("A, i> 1")
 
