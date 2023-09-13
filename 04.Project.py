81ead538a53773845e134070302537dbe8694ce9
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-13 14:23:21 +0000

    04.Project

--- 04.Project.py
+++ 04.Project.py
@@ -1,9 +1,11 @@
 N= int(input("Enter Start of Range:"))
 N2= int(input("Enter End of Range: "))
-P= ("Prime Numbers between N and N2: ")
-for i in range (N,N2//2 , +1):
-    if i > 1: 
-        prime = True
+for i in range (N, N//2, + 1):
+    result = i > 1 
+    
+
+     
+
          
         
     
