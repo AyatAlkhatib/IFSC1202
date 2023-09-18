cc0de38028c2e85c9235dd07e96a58de70093e83
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-18 15:33:55 +0000

    05.Project.py

---
+++ 05.Project.py
@@ -0,0 +1,8 @@
+a= (input("Enter Start Of Range: "))
+b= (input("Enter End Of Range: "))
+c= (input("Special Numbers between 1 and 10000"))
+for N in range (a,b):
+    if N == 0: 
+         for i in range (N//=10):
+         print (i) 
+         
