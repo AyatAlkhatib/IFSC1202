f28633f3e9dc06edf8f88c5b957dc5b0f99c13b5
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-20 15:20:55 +0000

    exam1.py

--- Exam One.py
+++ Exam One.py
@@ -1,21 +1,29 @@
 a= int(input("Enter Start of Range: "))
 b= int(input("Enter End of Range: "))
 print ("Super Special Numbers between", a, "and", b)
-for i in range (a, b + 1):
+for i in range (a, b + 1): 
     n=i
-    length = 0 
     while n > 0:
         n= n // 10
-        length = length +1
     n=i
-    results = 0 
-    while n > 0:
-        digits= n % 10
-        results = digits** length + results
-        n= n // 10
+    results = 0  
+    while n > 0: 
+        digits= n % 10 
+
+# compute factorial of digits
+        prod = 1
+        for  j in range (1, digits +1):
+            prod = prod * j
+
+
+        results = results + prod
+        n= n // 10 
     
-    if results == i:
+    if results == i: 
         print(i)
+
+
+
 
 
 
