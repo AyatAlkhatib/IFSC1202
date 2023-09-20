33a56679351eb45a24731a65962e311bfc8b7039
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-20 13:53:40 +0000

    05.project.py

--- 05.Project.py
+++ 05.Project.py
@@ -2,10 +2,10 @@ a= (input("Enter Start Of Range: "))
 b= (input("Enter End Of Range: "))
 c= (input("Special Numbers between 1 and 10000"))
 number_of_digits = 0
-for N in range (a,b)
-while N >0: 
-    N//=10 
-    number_of_digits + 1 
+for I in range (a,b+1):
+    N=I
+    while N>0 
+    number_of_digits= + 1 
     results = 0 
 if N>0: 
     digits= N % 10
