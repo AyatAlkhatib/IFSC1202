0ad7b6f198d98f3549c1839255b65b9b7761ce01
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-20 14:30:32 +0000

    Exam one.py

--- Exam One.py
+++ Exam One.py
@@ -3,12 +3,12 @@ b= (input("Enter End of Range: "))
 print("Super Special Numbers between:", a, "and", b)
 for i in range (a, b + 1): 
     length= 0  
-    i=n 
+    n=i          #n represents num 
     while n>0:
         n= n // 10
         length= length +1 
     
-    i=n
+    n=i
     results = 0  
     while n > 0: 
         digits= n % 10 
