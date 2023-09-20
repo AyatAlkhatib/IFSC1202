864be7bbec509cb9f777ede837844fe66c453ab7
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-20 14:43:38 +0000

    05.project.py

--- 05.Project.py
+++ 05.Project.py
@@ -1,22 +1,21 @@
 a= (input("Enter Start Of Range: "))
 b= (input("Enter End Of Range: "))
-print("Super Special Numbers between:, a, "and", b")
+print ("Super Special Numbers between", a, "and", b)
 for i in range (a, b + 1): 
     n=i
-    length= 0 
+    length = 0 
     while n > 0:
-        n= n//10
-        length= length +1 
-    
-    n=i 
+        n= n // 10
+        length = length +1 
+    n=i
     results = 0  
     while n > 0: 
         digits= n % 10 
         results = results + digits** length 
-        n= n// 10 
+        n= n // 10 
     
     if results == i: 
-        print (i)
+        print(i)
 
 
 
--- Exam One.py
+++ Exam One.py
@@ -1,23 +1,21 @@
 a= (input("Enter Start of Range: "))
 b= (input("Enter End of Range: "))
-print("Super Special Numbers between:", a, "and", b)
+print ("Super Special Numbers between", a, "and", b)
 for i in range (a, b + 1): 
-    length= 0  
-    n=i          #n represents num 
-    while n>0:
+    n=i
+    length = 0 
+    while n > 0:
         n= n // 10
-        length= length +1 
-    
+        length = length +1 
     n=i
     results = 0  
     while n > 0: 
         digits= n % 10 
         results = results + digits** length 
-        n= n// 10 
+        n= n // 10 
     
     if results == i: 
         print(i)
-
 
 
 
