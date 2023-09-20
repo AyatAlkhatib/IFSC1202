a037234033814c8747c2af4fda2e800411cd71ca
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-20 15:23:11 +0000

    05.project.py

--- 05.Project.py
+++ 05.Project.py
@@ -3,7 +3,7 @@ b= int(input("Enter End Of Range: "))
 print ("Special Numbers between", a, "and", b)
 for i in range (a, b + 1): 
     n=i
-    length = 0 
+    length = 0                             # we want the length to calculate power of the digits 
     while n > 0:
         n= n // 10
         length = length +1 
@@ -11,7 +11,7 @@ for i in range (a, b + 1):
     results = 0  
     while n > 0: 
         digits= n % 10 
-        results = results + digits** length 
+        results = results + digits** length       # we want to find the whole results of the digitsl to the power of given length
         n= n // 10 
     
     if results == i: 
