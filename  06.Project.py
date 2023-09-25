80f3e05896285e6bf5766c531ee0a3db05c544af
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-25 15:27:25 +0000

    06.Project.py

---  06.Project.py
+++  06.Project.py
@@ -1,11 +1,12 @@
 inputfilename = "06.Project Input File.txt" 
-outputfilename = "06.Project Output File.txt"
 mergefilename ="06.Project Merge File.txt"
+outputfilename = "06.Project Output File.txt"
 # Open the input file for reading
 with open(inputfilename, 'r') as inputfile: 
     # open the merge file for reading 
     with open(mergefilename, 'r') as mergefile: 
         # Open the output file for writing
         with open(outputfilename, 'w') as outputfile: 
-             
+              recordcount= 0 
+
 
