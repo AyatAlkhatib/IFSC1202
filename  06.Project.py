aabc61db7f9679f5512f99ba0205a100ad32520f
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-25 14:53:40 +0000

    06.Project.py

---
+++  06.Project.py
@@ -0,0 +1,8 @@
+inputfilename = "06.Project Input File.txt" 
+outputfilename = "06.Project Output File.txt"
+mergefilename ="06.Project Merge File.txt"
+# Open the input file for reading
+inputfile = open(inputfilename, 'r')
+# Open the output file for writing
+outputfile = open(outputfilename, 'w')  
+
