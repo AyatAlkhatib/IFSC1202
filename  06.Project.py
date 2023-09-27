971f51349065033ca402cdad66a19a2beea1fa43
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-09-27 14:42:05 +0000

    saved

---  06.Project.py
+++  06.Project.py
@@ -3,13 +3,28 @@ inputfilename = "06.Project Input File.txt"
 outputfilename ="06.Project Output File.txt"
 # Open the input file for reading
 inputfile = open("06.Project Input File.txt", "r") 
- # Open the output file for writing
+# Open the output file for writing
 outputfile = open("06.Project Output File.txt", "w") 
 #file records
 inputrecords = 0
 outputrecords= 0 
-mergerecords = 0
-#Read the first line of the input file
-for line in the inputfile: 
+mergerecords = 0 
+for line in inputfile:
     inputrecords +=1
-    if ""
+    if "**Insert Merge File Here**":
+        mergefile = open ("06.Project Merge File.txt")
+        for line in mergefile: 
+            mergerecords +=1 
+            for line in outputfile: 
+                if "**Insert Merge File Here**": 
+                    outputrecords +=1 
+inputfile.close 
+outputfile.close
+
+print ({inputrecords}, inputfile)
+print({mergerecords}, mergefile)
+print ({outputrecords}, outputfile)
+
+
+
+
