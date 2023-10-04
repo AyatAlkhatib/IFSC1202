6790c0d417e6d97b12f46225ebb6ba8516b76cd1
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-10-04 14:09:26 +0000

    save

---  06.Project.py
+++  06.Project.py
@@ -1,5 +1,8 @@
+# Open Input File for Reading
 inputfile = open("06.Project Input File.txt", "r") 
+# Open Output File for Writing
 outputfile = open("06.Project Output File.txt", "w") 
+# File Records
 inputrecords = 0
 outputrecords= 0 
 mergerecords = 0 
@@ -17,8 +20,10 @@ while line !="":
        outputrecords +=1 
        inputrecords +=1
     line = inputfile.readline()
+# Close all the Files
 inputfile.close()
 outputfile.close() 
+# Print the records of the Files
 print(inputrecords, "Input File Records")
 print(outputrecords, "Output File Records")
 print(mergerecords, "Merge File Records")
