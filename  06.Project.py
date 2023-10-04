b2561b475c977a50818b55ee23858956be4c2206
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-10-04 15:00:59 +0000

    06.project.py

---  06.Project.py
+++  06.Project.py
@@ -1,20 +1,22 @@
 # Open Input File for Reading
-inputfile = open("06.Project Input File.txt", "r") 
+inputfile = open("06.Project Input File.txt.md", "r") 
 # Open Output File for Writing
-outputfile = open("06.Project Output File.txt", "w") 
+outputfile = open("06.Project Output File.txt.md", "w") 
 # File Records
 inputrecords = 0
 outputrecords= 0 
 mergerecords = 0 
 line= inputfile.readline()
 while line !="":
-    if line == "**Insert Merge File Here**":
-        mergefile = open ("06.Project Merge File.txt", "r")
-        mergefilename= mergefile.readline()
-        while mergefilename !="":
-           outputfile.write(mergefilename)  
-           outputrecords +=1 
-        outputfile.write("\n")
+    if line == "**Insert Merge File Here**\n":
+        mergefile = open ("06.Project Merge File.txt.md", "r")
+        line2= mergefile.readline()
+        while line2 !="":
+           outputfile.write(line2)
+           line2 = mergefile.readline()
+           outputrecords +=1
+           mergerecords +=1 
+        outputfile.write("/n")
         mergefile.close()
     else: 
        outputfile.write(line)
