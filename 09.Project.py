b85112fe2763c9ccf6af2952f61688090eb4714a
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-10-25 01:47:46 +0000

    saved

--- 09.Project.py
+++ 09.Project.py
@@ -2,7 +2,7 @@
 a = []
 # Open the file and read the first line
 csvFile = open("09.Project Distances.csv")
-x = csvFile.readline(0).strip()
+x = csvFile.readline().strip()
 # While not at end of file 
 while x != "":
 # Split the line into a list
@@ -19,7 +19,34 @@ for i in range(len(a)):
 # Loop though each element in the list
     for j in range(len(a[i])):
 # Print each element in the list
-        print(a[i][j], end=' ')
+        print('{:>10}'.format(a[i][j]), end=' ')
 # End of list - go to next line
-    print()
-	
+    print() 
+
+
+
+from_city = input("From City: ")
+to_city= input("To City: ")
+from_city_row = 0 
+to_city_row = 0 
+
+
+for i in range(len(a)): 
+      if a[i][0] == from_city:
+            from_city = i 
+            break 
+for i in range (len(a[0])):
+      if a[0][i] == to_city: 
+            to_city_colum = i 
+            break 
+
+for i in range(len(a)):
+      if from_city_row == 0:
+            print("Invalid to City")
+            break 
+for i in range(len(a[0])):
+      if to_city_colum == 0: 
+            print("Invalid to City")
+            break 
+#print 
+print(from_city, "to", to_city , "-", a[from_city_row][to_city_colum], "Miles")
