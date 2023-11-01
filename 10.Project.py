fd412434010d840f6b2e55187040e7d62fcf8a0c
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-11-01 14:50:05 +0000

    saved

--- 10.Project.py
+++ 10.Project.py
@@ -1,50 +1,49 @@
 
 # Step 1 - Create a class called "Student"
 class Students ():
-
-# Step 2 - Define the initializer, the initializer should accept the folloing class values: 
- def __init__(self, firstname, lastname, tnumber, scores): 
+ # Step 2 - Define the initializer, the initializer should accept the folloing class values: 
+  def __init__(self, firstname, lastname, tnumber, scores): 
      self.FirstName = firstname
      self.LastName = lastname
      self.TNumber = tnumber 
      self.Grades = [int(score) if score.strip() else 0 for score in scores]
-
-# Step 4 - Define the methods for the object
-def RunningAverage(self): 
+ # Step 4 - Define the methods for the object
+  def RunningAverage(self): 
     RunningAverage = [scores for scores in self.Grades if scores != 0]
     if RunningAverage: 
         return sum(RunningAverage)/len(RunningAverage)
     else: 
         return 0 
-
-
-def TotalAverage(self): 
+  def TotalAverage(self): 
     return sum(self.Grades) / len(self.Grades)
-
-def LetterGrade(self): 
-    if Average >= 90:
-        return 'A'
-    if 80 <= Average < 90: 
+  
+  def LetterGrade(self): 
+     Average = self.TotalAverage()
+     if Average >= 90:
+         return 'A'
+     if 80 <= Average < 90: 
         return 'B'
-    if 70 <= Average < 80:
+     if 70 <= Average < 80:
         return 'C'
-    if 60 <= Average < 70:
+     if 60 <= Average < 70:
         return 'D'
-    if Average < 60: 
+     if Average < 60: 
         return 'f'
     
 # Step 5 -  Create 4 instances of students
-mystudent1 = Students("Jim","Evans","T123456", 3)
-mystudent2 = Students("Joe","Smith","T654321", 4)
-mystudent3 = Students("Jane","Doe", "T121212",2)
-mystudent4 = Students()
+mystudent1 = Students("Jim","Evans","T123456", [95, 0, 71])
+mystudent2 = Students("Joe","Smith","T654321", [90, 80, 85, 97])
+mystudent3 = Students("Jane","Doe", "T121212",[0, 100, 99])
+
+# create array 
+Students = [mystudent1, mystudent2, mystudent3]
 
 # Read File 
 StudentsFiles = open ("10.Project Student Scores.tx")
 for line in StudentsFiles:
         data = line.strip().split(",")
         firstname, lastname, tnumber, *scores = data
-        student = Students(firstname, lastname, tnumber, scores)
+        student = Students(firstname, lastname, tnumber, scores(0))
         Students.append(student)
 
 #Print the attributes
