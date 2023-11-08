4e35d1cae45ba01232ed974e83192b54a406b62a
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2023-11-08 15:41:55 +0000

    saved

--- 10.Project.py
+++ 10.Project.py
@@ -9,43 +9,43 @@ class Students ():
      self.Grade = scores
  # Step 4 - Define the methods for the object
   def RunningAverage(self): 
-    total = 0 
-    count = 0 
-    for i in range (len(self.Grade)):
-       if self.Grade[i].strip()!=" ": 
-          total = total + float(self.Grade[i].strip())
-          count = count + 1 
-    return total / count  
+      total = 0 
+      count = 0 
+      for i in range (len(self.Grade)):
+         if self.Grade[i].strip() != "": 
+            total = total + float(self.Grade[i].strip())
+            count = count + 1 
+      return total / count  
+  
   def TotalAverage(self): 
-    total = 0 
-    for i in range(len(self.Grade)):
-       if self.Grade[i].strip()!= "": 
-          total = total +float(self.Grade[i].strip())
-
-    return total / len(self.Grade)
+      total = 0 
+      for i in range(len(self.Grade)):
+         if self.Grade[i].strip() != "": 
+          total = total + float(self.Grade[i].strip())
+      return total / len(self.Grade)
   
   def LetterGrade(self): 
      Average = self.TotalAverage()
      if Average >= 90:
          return 'A'
-     if 80 <= Average < 90: 
+     if Average >= 80: 
         return 'B'
-     if 70 <= Average < 80:
+     if Average >=70:
         return 'C'
-     if 60 <= Average < 70:
+     if Average >= 60:
         return 'D'
-     if Average < 60: 
-        return 'f'
+     
+     return 'F'
     
 # Read File 
-StudentsFiles = open ("10.Project Student Scores.tx")
-print("({:>12s} {:>12s} {:>12} {:>12} {:>12} {:>12})".format("First" , "Last" , "ID" , "Running" , "Semster" , "Letter"))
-print("({:>12s} {:>12s} {:>12} {:>12} {:>12} {:>12})".format("Name"  , "Name" , "Number", "Average" ,"Average", "Grade"))
-print("({:>12s} {:>12s} {:>12} {:>12} {:>12} {:>12})".format("-"*12, "-"*12  , "-"*12   , "-"*12  ,  "-"*12  , "-"*12 ))
-x= StudentsFiles.readline()
+StudentsFiles = open ("10.Project Student Scores.txt", "r")
+print("{:>12s} {:>12s} {:>12s} {:>12s} {:>12s} {:>12s}".format("First", "Last", "ID", "Running", "Semester", "Letter"))
+print("{:>12s} {:>12s} {:>12s} {:>12s} {:>12s} {:>12s}".format("Name", "Name", "Number", "Average" ,"Average", "Grade"))
+print("{:>12s} {:>12s} {:>12s} {:>12s} {:>12s} {:>12s}".format("-"*12, "-"*12, "-"*12 ,"-"*12, "-"*12, "-"*12 ))
+x= StudentsFiles.readline()   
 
 while x != "": 
-   y=x.split(",")
-   Student = Students (y[0].strip(),y[1].strip(),y[2].strip(),y[3].strip())
-   print("({:>12s} {:>12s} {:>12} {:>12} {:>12} {:>12})".format(Students.FirstName, Students.LastName, Students.TNumber, Students.RunningAverage(), Students.TotalAverage(), Students.LetterGrade()))
-
+   y= x.split(",")
+   Student = Students (y[0].strip(),y[1].strip(),y[2].strip(),y[3:])
+   print("{:>12s} {:>12s} {:>12s} {:>12.2f} {:>12.2f} {:>12s}".format(Student.FirstName, Student.LastName, Student.TNumber, Student.RunningAverage(), Student.TotalAverage(), Student.LetterGrade()))
+   x= StudentsFiles.readline()
